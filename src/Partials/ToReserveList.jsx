46655f226d08092d423efbb72bedcf63b7e71bb4
Ken <kel89@cornell.edu>
2023-12-05 17:27:27 -0700

    added some sorting to reserveList"

--- src/Partials/ToReserveList.jsx
+++ src/Partials/ToReserveList.jsx
@@ -4,6 +4,30 @@ import ReservationCard from "./ReservationCard";
 export default function ToReserveList({resList, deleteReservation, working}) {
 
 
+  const sortFn = (a, b) => {
+    // Convert date strings to Date objects
+    const dateA1 = new Date(a.reserveTarget);
+    const dateB1 = new Date(b.reserveTarget);
+
+    if (dateA1 < dateB1) {
+      return -1;
+    } else if (dateA1 > dateB1) {
+      return 1;
+    } else {
+      // If reserveTarget is equal, compare d2
+      const dateA2 = new Date(a.reserveOn);
+      const dateB2 = new Date(b.reserveOn);
+
+      if (dateA2 < dateB2) {
+        return -1;
+      } else if (dateA2 > dateB2) {
+        return 1;
+      } else {
+        return 0; // If both reserveTarget and reserveOn are equal
+      }
+    }
+  }
+
   return (
     <div className="w-11/12 max-w-3xl">
       <h1 className="text-lg font-semibold text-center">
@@ -15,7 +39,7 @@ export default function ToReserveList({resList, deleteReservation, working}) {
         </div>
       ) : (
         <div className="w-full flex flex-col gap-3">
-          {resList.map((res) => (
+          {resList.sort(sortFn).map((res) => (
             <ReservationCard
               reservation={res}
               deleteReservation={deleteReservation}
